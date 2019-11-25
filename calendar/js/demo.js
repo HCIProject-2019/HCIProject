3130bebe8627375777506b47561b158ed77543f5
emily0906 <42950390+emily0906@users.noreply.github.com>
2019-11-24 20:37:12 -0600

    change

--- calendar/js/demo.js
+++ calendar/js/demo.js
@@ -1,7 +1,5 @@
 var events = [
-  {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-  {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-  {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+  {'Date': new Date(2019, 10, 24), 'Title': 'Disney on Ice', 'Link':'../events/eventpage.html'},
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
